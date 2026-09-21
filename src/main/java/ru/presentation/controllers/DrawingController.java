85c1eb9231ea8a80cc7c7b485d2abfe60f26d7ea
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2026-09-21 21:00:51 +0300

    init

--- src/main/java/ru/presentation/controllers/DrawingController.java
+++ src/main/java/ru/presentation/controllers/DrawingController.java
@@ -1,12 +1,11 @@
 package ru.presentation.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.presentation.dto.DrawingDto;
-
-import java.io.File;
-import java.util.Base64;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/drawings")
