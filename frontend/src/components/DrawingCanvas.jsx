409c3d9b563a6ecbcfddc8229042cf2add6f4a05
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2026-09-22 11:11:04 +0300

    templates

--- frontend/src/components/DrawingCanvas.jsx
+++ frontend/src/components/DrawingCanvas.jsx
@@ -32,7 +32,7 @@ const DrawingCanvas = forwardRef(function DrawingCanvas({ initialSlides, onSlide
             <canvas
                 ref={canvasRef}
                 width={900}
-                height={339}
+                height={356}
                 className="drawing-canvas"
                 onPointerDown={editor.startDrawing}
                 onPointerMove={editor.draw}
--- src/main/java/ru/presentation/services/PptxGeneratorService.java
+++ src/main/java/ru/presentation/services/PptxGeneratorService.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class PptxGeneratorService {
 
     private static final int PREVIEW_WIDTH = 900;
-    private static final int PREVIEW_HEIGHT = 339;
+    private static final int PREVIEW_HEIGHT = 356;
 
     private final TemplateRepository templateRepository;
 
